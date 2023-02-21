23d75a86f6a574f619cb090f4ab60399fc62dd92
vipinrwt95 <vipinrwt9@gmail.com>
2023-02-21 12:26:04 +0530

    req.body

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 
 const express=require('express');
+const bodyParser=require('body-parser');
+
 
 const app=express();
-app.use((req,res,next)=>{
-    console.log("middleware")
-    next();
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/add-product',(req,res,next)=>{
+res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text"name="size"><button type="submit">Add product</button></form>');
 });
-app.use((req,res,next)=>{
-    console.log("second middleware")
-    res.send({name:'vipin'});
+app.post('/product',(req,res)=>{
+   console.log(req.body);
+   res.redirect('/');
+})
+app.use('/',(req,res,next)=>{
+ 
+    res.send('<h1>Hello from Express</h1>');
 });
 
 app.listen(5000)
