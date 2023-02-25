8b50291bce646b370d74c29a77a405087d31cfd5
vipinrwt95 <vipinrwt9@gmail.com>
2023-02-25 20:39:55 +0530

    adding css

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,22 @@
 const express=require('express');
-
+const path=require('path');
+const rootDir=require('../utility/path');
 const router=express.Router();
 
 router.get('/',(req,res,next)=>{
-  res.send('<h1>Welcome to Express.js</h1>')
+  res.sendFile(path.join(rootDir,'views','shop.html'));
 })
-
+router.get('/contactus',(req,res,next)=>{
+  res.sendFile(path.join(rootDir,'views','contact.html'));
+})
+router.post('/success',(req,res,next)=>{
+    if(req.body.emailId.includes('@'))
+    {
+      res.send('<h1>Form Successfully filled</h1>');
+    }
+    else
+    {
+      res.redirect('/contactus')
+    }
+  })
 module.exports=router;
