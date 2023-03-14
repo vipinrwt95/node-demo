023a4bc3437816aaaa18936e33c85e6c376b13b7
vipinrwt95 <vipinrwt9@gmail.com>
2023-03-14 20:18:11 +0530

    booking appointment full stack

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const sequelize=require('./util/database');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors=require('cors')
 
 const errorController = require('./controllers/error');
 
@@ -9,20 +10,22 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
+app.use(cors());
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const userRoutes =require('./routes/user')
 
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(userRoutes);
 
 app.use(errorController.get404);
 sequelize.sync().then(result=>{
-    console.log(result)
+   
     app.listen(3000);
 })
 .catch(err=>{console.log(err)})
